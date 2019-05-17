91de81e146b7c0160557f30bfddcfd67796bf8bd
Sebastian Grugel <sebastian.grugel@exea.local>
2019-05-17 14:37:08 +0200

    NFS storage is creating

--- Learning/vsphere_example1/modules/resources.tf
+++ Learning/vsphere_example1/modules/resources.tf
@@ -10,6 +10,65 @@ data "vsphere_datacenter" "old_datacenter" {
   name = "EXEA-DC-${var.environment}"
   }
 
+
+
+// Tworzenie jednego datasotra z 3 znalezionych dysków dla pierwszego ESXi
+data "vsphere_vmfs_disks" "available1" {
+  host_system_id = "${data.vsphere_host.hosts.0.id}"
+  rescan         = true
+  filter         = "naa.60"
+}
+
+resource "vsphere_vmfs_datastore" "datastore1" {
+  name           = "Datastore-${data.vsphere_host.hosts.0.name}_${var.environment}_Local"
+  host_system_id = "${data.vsphere_host.hosts.0.id}"
+  //folder         = "datastore-folder"
+  disks = ["${data.vsphere_vmfs_disks.available1.disks}"]
+}
+
+// Tworzenie jednego datasotra z 3 znalezionych dysków dla drugiego ESXi
+data "vsphere_vmfs_disks" "available2" {
+  host_system_id = "${data.vsphere_host.hosts.1.id}"
+  rescan         = true
+  filter         = "naa.60"
+}
+
+resource "vsphere_vmfs_datastore" "datastore2" {
+  name           = "Datastore-${data.vsphere_host.hosts.1.name}_${var.environment}_Local"
+  host_system_id = "${data.vsphere_host.hosts.1.id}"
+  //folder         = "datastore-folder"
+  disks = ["${data.vsphere_vmfs_disks.available2.disks}"]
+}
+
+
+
+
+resource "vsphere_nas_datastore" "nfsdatastore" {
+  name            = "terraform-testnfs"
+  host_system_ids = ["${data.vsphere_host.hosts.*.id}"]
+  type         = "NFS"
+  remote_hosts = ["10.3.5.62"]
+  remote_path  = "/mnt/nfs/nfs1"
+}
+
+
+
+
+
+//To działa na jednego hosta i konkretny dysk
+/*resource "vsphere_vmfs_datastore" "datastore" {
+  name           = "terraform-test"
+    host_system_id = "${data.vsphere_host.hosts.0.id}"
+  disks = [
+    "naa.6000c29c55aaa3b7b04b086b666a2535",
+      ]
+}
+
+*/
+
+
+
+
 // Tworzenie klastra hostów
 resource "vsphere_compute_cluster" "compute_cluster" {
   name            = "Terraform${var.company}_Cluster-VMUG_${var.environment}"
@@ -23,7 +82,7 @@ resource "vsphere_compute_cluster" "compute_cluster" {
 
 // Tworzenie DVS
 resource "vsphere_distributed_virtual_switch" "dvs" {
-  name          = "VDS-${var.company}-MGMT"
+  name          = "VDS-${var.company}-${var.environment}-MGMT"
   datacenter_id = "${data.vsphere_datacenter.old_datacenter.id}"
   uplinks         = ["uplink1"]
   
@@ -40,10 +99,9 @@ resource "vsphere_distributed_virtual_switch" "dvs" {
 }
 
 resource "vsphere_distributed_virtual_switch" "dvs_nsx" {
-  name          = "VDS-${var.company}-NSX"
+  name          = "VDS-${var.company}-${var.environment}-NSX"
   datacenter_id = "${data.vsphere_datacenter.old_datacenter.id}"
 }
-
 
 
 
@@ -51,28 +109,28 @@ resource "vsphere_distributed_virtual_switch" "dvs_nsx" {
 // Tworzenie portgrup na VDS
 
  resource "vsphere_distributed_port_group" "pg_mgmt" {
-  name = "PG-${var.company}-MGT"
+  name = "PG-${var.company}-${var.environment}-MGT"
   distributed_virtual_switch_uuid = "${vsphere_distributed_virtual_switch.dvs.id}"
   vlan_id = 1001
   }
  resource "vsphere_distributed_port_group" "pg_backup" {
-  name = "PG-${var.company}-BACKUP"
+  name = "PG-${var.company}-${var.environment}-BACKUP"
   distributed_virtual_switch_uuid = "${vsphere_distributed_virtual_switch.dvs.id}"
   vlan_id = 1002
     
 }
 resource "vsphere_distributed_port_group" "pg_repl" {
-  name = "PG-${var.company}-REPLICATION"
+  name = "PG-${var.company}-${var.environment}-REPLICATION"
   distributed_virtual_switch_uuid = "${vsphere_distributed_virtual_switch.dvs.id}"
   vlan_id = 1003
 }
 resource "vsphere_distributed_port_group" "pg_vmotion" {
-  name = "PG-${var.company}-VMOTION"
+  name = "PG-${var.company}-${var.environment}-vMOTION"
   distributed_virtual_switch_uuid = "${vsphere_distributed_virtual_switch.dvs.id}"
   vlan_id = 1004
 }
 resource "vsphere_distributed_port_group" "pg_vsan" {
-  name = "PG-${var.company}-VSAN"
+  name = "PG-${var.company}-${var.environment}-VSAN"
   distributed_virtual_switch_uuid = "${vsphere_distributed_virtual_switch.dvs.id}"
   vlan_id = 1005
 }
@@ -84,17 +142,17 @@ resource "vsphere_distributed_port_group" "pg_vsan" {
 
 //Tworzymy ResourcePool // Creating Resource Pool
 resource "vsphere_resource_pool" "resource_pool_priority" {
-  name = "Mission Critical VMs"
+  name = "Mission Critical VMs - ${var.environment}"
   parent_resource_pool_id = "${vsphere_compute_cluster.compute_cluster.resource_pool_id}"
     depends_on = ["vsphere_compute_cluster.compute_cluster"]
 }
 resource "vsphere_resource_pool" "resource_pool_regular" {
-  name = "Regular VMs"
+  name = "Regular VMs - ${var.environment}"
   parent_resource_pool_id = "${vsphere_compute_cluster.compute_cluster.resource_pool_id}"
     depends_on = ["vsphere_compute_cluster.compute_cluster"]
 }
 resource "vsphere_resource_pool" "resource_pool_mgmt" {
-  name = "Management"
+  name = "Management - ${var.environment}"
   parent_resource_pool_id = "${vsphere_compute_cluster.compute_cluster.resource_pool_id}"
     depends_on = ["vsphere_compute_cluster.compute_cluster"]
 }
@@ -150,3 +208,5 @@ resource "vsphere_folder" "teamfolder" {
 datacenter_id = "${data.vsphere_datacenter.old_datacenter.id}"
 count = 5
 }
+
+
