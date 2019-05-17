df440b4c628b697363c3385358349bfc94bd0b44
Sebastian Grugel <sebastian.grugel@exea.local>
2019-05-17 16:25:05 +0200

    ,

--- Learning/vsphere_example1/environments/EXEA-DC-PROD/module_call.tf
+++
@@ -1,7 +0,0 @@
-module "dev_environment" {
-    source = "../../modules"
-    hosts = ["sg-esxi03.exea.dev","sg-esxi04.exea.dev"]
-    environment = "PROD"
-    network_interfaces = ["vmnic1"]
-    company = "EXEA"
-}
